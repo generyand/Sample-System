892a9005bc82ff29ed76a443036e39f7bdf30586
generyand <110978525+generyand@users.noreply.github.com>
2023-10-02 15:17:44 +0800

    Update edit_page.php

--- edit_page.php
+++ edit_page.php
@@ -12,9 +12,28 @@ $new_id = $info_row['stud_id'];
 
 ?>
 
-<form action="edit_function.php?cd=<?php echo $new_id;?>" method="POST">
-    <h3>Your info</h3>
-    <input type="text" name="student_name" value="<?php echo $info_row['student_name'] ?>">
-    <input type="text" name="student_course" value="<?php echo $info_row['student_course'] ?>">
-    <button type="submit" name="update">Update</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Page</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="edit-container border-radius box-shadow">
+        <h3 class="primary-heading ">YOUR INFO</h3>
+        <form class="main-padding gap-24" action="edit_function.php?cd=<?php echo $new_id; ?>" method="POST">
+            <div class="search-wrapper gap-24">
+                <input type="text" name="student_name" value="<?php echo $info_row['student_name'] ?>">
+                <input type="text" name="student_course" value="<?php echo $info_row['student_course'] ?>">
+                <button id="update-btn" type="submit" name="update">UPDATE</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
